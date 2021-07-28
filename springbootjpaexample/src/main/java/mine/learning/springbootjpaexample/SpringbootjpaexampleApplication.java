af1e376dacbe34d1d93cfbada3d4a14a2f87fae2
Kacper <kacpergierycz@gmail.com>
2021-07-28 09:37:51 +0200

    Project initialize and properties set

---
+++ springbootjpaexample/src/main/java/mine/learning/springbootjpaexample/SpringbootjpaexampleApplication.java
@@ -0,0 +1,13 @@
+package mine.learning.springbootjpaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjpaexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjpaexampleApplication.class, args);
+	}
+
+}
