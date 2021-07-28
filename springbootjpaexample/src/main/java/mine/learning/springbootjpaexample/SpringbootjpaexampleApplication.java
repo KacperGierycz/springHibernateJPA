c7ffe5287c28aa2388a9e63162a15ee765f27040
Kacper <kacpergierycz@gmail.com>
2021-07-28 14:41:59 +0200

    entity manager only

--- springbootjpaexample/src/main/java/mine/learning/springbootjpaexample/SpringbootjpaexampleApplication.java
+++ springbootjpaexample/src/main/java/mine/learning/springbootjpaexample/SpringbootjpaexampleApplication.java
@@ -6,6 +6,7 @@ import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceUnit;
 
 import org.springframework.boot.SpringApplication;
@@ -16,9 +17,12 @@ import mine.learning.springbootjpaexample.models.EmployeeLimited;
 @SpringBootApplication
 public class SpringbootjpaexampleApplication {
 	
-	@PersistenceUnit
-	private EntityManagerFactory emf;
+//	@PersistenceUnit
+//	private EntityManagerFactory emf;
 
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootjpaexampleApplication.class, args);
 	
@@ -28,19 +32,21 @@ public class SpringbootjpaexampleApplication {
 	
 	@PostConstruct
 	public void start() {
-		EmployeeLimited e = new EmployeeLimited();
-		e.setAge(20);
-		e.setDob(new Date());
-		e.setName("Fello");
-		e.setSsn("12344");
+//		EmployeeLimited e = new EmployeeLimited();
+//		e.setAge(20);
+//		e.setDob(new Date());
+//		e.setName("Fello");
+//		e.setSsn("12344");
 		
-		EntityManager entityManager = emf.createEntityManager();
-		EntityTransaction transaction = entityManager.getTransaction();
-		transaction.begin();
-		entityManager.persist(e);
-		transaction.commit();
-		entityManager.clear();
+		//EntityManager entityManager = emf.createEntityManager();
+//		EntityTransaction transaction = entityManager.getTransaction();
+//		transaction.begin();
+//		entityManager.persist(e);
+//		transaction.commit();
+//		entityManager.clear();
 		
+		EmployeeLimited employee = entityManager.find(EmployeeLimited.class, 1);
+		System.out.println(employee);
 		
 	}
 
